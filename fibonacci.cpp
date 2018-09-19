8d1475f8205c0fdd15265aef3446c9382e558fb3
5981 <tregubov@phystech.edu>
2018-09-19 10:10:34 +0300

    seeded sequence

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -6,26 +6,22 @@
 // we calculate Fibonacci numbers using 3 different ways
 
 // recursion
-template<class T>
-T Fibonacci(const uint32_t n) {
+template<typename T>
+T Fibonacci(const uint32_t n, const std::vector<T>& seed) {
   if (n < 2) {
-    return n;
+    return seed[n];
   }
-  return Fibonacci<T>(n-1) + Fibonacci<T>(n-2);
+  return Fibonacci<T>(n-1, seed) + Fibonacci<T>(n-2, seed);
 }
 
 // recursion with memory
-template<class T>
+template<typename T>
 class Fib {
 public:
-  Fib() {
-    results_.push_back(0);
-    results_.push_back(1);
+  Fib(const std::vector<T>& seed) {
+    this->results_ = std::vector<T>(seed);
   }
   T operator()(const uint32_t n) {
-    if (n < 2) {
-      return n;
-    }
     if (n < results_.size()) {
       return results_[n];
     }
@@ -36,21 +32,31 @@ private:
 };
 
 // expression
-uint32_t F(const uint32_t n) {
-  if (n < 2) {
-    return n;
-  }
+template<typename T>
+uint32_t F(const uint32_t n, const std::vector<T>& seed) {
   const double sqrt5 = std::sqrt(5);
   const double phi = (1 + sqrt5) / 2;
-  return static_cast<uint32_t>(std::pow(phi, n)/sqrt5 + 0.5);
+  const double psi = (1 - sqrt5) / 2;
+  return std::round((seed[0]*phi-seed[1])/sqrt5 * std::pow(psi, n) -
+                    (seed[0]*psi-seed[1])/sqrt5 * std::pow(phi, n));
 }
 
 int main() {
   uint32_t n;
   std::cout << "Enter the ordinal number of the Fibonacci number\n";
   std::cin >> n;
-  std::cout << Fibonacci<int>(n) << std::endl;
-  Fib<int> fib;
+
+  // initialise Fibonacci sequence
+  std::vector<uint32_t> seed(2);
+  seed[0] = 3;
+  seed[1] = 4;
+
+  std::cout << Fibonacci<uint32_t>(n, seed) << std::endl;
+
+  Fib<uint32_t> fib(seed);
   std::cout << fib(n) << std::endl;
-  std::cout << F(n) << std::endl;
+
+  std::cout << F<uint32_t>(n, seed) << std::endl;
+  
+  return 0;
 }
